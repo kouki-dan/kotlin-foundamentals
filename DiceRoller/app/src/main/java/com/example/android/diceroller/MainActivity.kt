312a330cb45a73981175ecbf1a7835d0cdc6b7e5
Kouki Saito <dan.addr.skd@gmail.com>
2019-06-24 18:51:32 +0900

    01.3 new DiceRoll

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,16 +3,29 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    var currentDice: Int? = null
+    val drawableDiceResource: Int
+        get() = when (currentDice ?: 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
@@ -27,25 +40,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        currentDice = randomInt
+        diceImage.setImageResource(drawableDiceResource)
     }
 
     private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
         var incrementedNumber: Int
-        try {
-            incrementedNumber = Integer.parseInt(resultText.text.toString()) + 1
+        val currentDice = currentDice
+        if (currentDice != null) {
+            incrementedNumber = currentDice + 1
             if (incrementedNumber >= 7) {
                 incrementedNumber = 1
             }
-        } catch (_: NumberFormatException) {
+        } else {
             incrementedNumber = 1
         }
+        this.currentDice = incrementedNumber
 
-        resultText.text = incrementedNumber.toString()
+        diceImage.setImageResource(drawableDiceResource)
     }
 }
